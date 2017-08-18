11b40a8e381fd3449a65bf8af79be7e9720b4251
javadoug <doug@onvelocity.com>
2017-08-18 00:29:27 -0400

    wip

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ console.log('Hello, world!');
 
 //http://blog.npmjs.org/post/118810260230/building-a-simple-command-line-tool-with-npm
 
-const shell = require('shelljs');
-
-shell.exec("./node_modules/build-scripts/node_modules/gulp/bin/gulp.js ./node_modules/build-scripts/gulpfile.js", function (error, stdout, stderr) {
-	console.log('stdout: ' + stdout);
-	console.log('stderr: ' + stderr);
-	if (error !== null) {
-		console.log('exec error: ' + error);
-	}
-});
+// const shell = require('shelljs');
+//
+// shell.exec("./node_modules/build-scripts/node_modules/gulp/bin/gulp.js ./node_modules/build-scripts/gulpfile.js", function (error, stdout, stderr) {
+// 	console.log('stdout: ' + stdout);
+// 	console.log('stderr: ' + stderr);
+// 	if (error !== null) {
+// 		console.log('exec error: ' + error);
+// 	}
+// });
