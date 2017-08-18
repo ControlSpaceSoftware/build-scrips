4bf41f0806d8963af8de4d7b4e9cc7a5466b86ee
javadoug <doug@onvelocity.com>
2017-08-17 23:59:30 -0400

    getting it working

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
 console.log('Hello, world!');
 
+
+
 const child_process = require('child_process');
+
+child_process.exec("npm install -g", function (error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
+
 child_process.exec("./node_modules/build-scripts/node_modules/gulp/bin/gulp.js ./node_modules/build-scripts/gulpfile.js", function (error, stdout, stderr) {
 	console.log('stdout: ' + stdout);
 	console.log('stderr: ' + stderr);
