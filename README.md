898869a4af1551a18cc42714c6800fdd45cfb7ac
javadoug <doug@onvelocity.com>
2017-08-20 13:21:45 -0400

    add more commands

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # build-scripts
-build scripts
+
+Centralized development commands for building ES6 modules and common development tasks.
+
+## Install
+
+```
+// locally
+npm install github:ControlSpaceSoftware/build-scripts.git --save-dev
+
+// globally
+npm install github:ControlSpaceSoftware/build-scripts.git -g
+```
+
+### Locally Installed Usage
+
+```
+./node_modules/build-scripts <cmd> <options>
+```
+
+### Globally Installed Usage
+
+```
+build-scripts <cmd> <options>
+```
+
+## Commands
+
+```
+build-scripts -h
+
+  Usage: index [options] [command]
+
+
+  Options:
+
+    -V, --version  output the version number
+    -h, --help     output usage information
+
+
+  Commands:
+
+    clean           remove ./dist directory and ./dist.zip file
+    zip             zip ./dist directory into ./dist.zip file
+    build-lambda    build lambda function in /dist directory
+    upload-lambda   build and deploy function to aws lambda - function must already exist
+    build           runs babel src -o lib // src/es6 to lib/es5
+    test            runs mocha test
+    test-watch      runs mocha test with the watch option
+    commit          runs build, test and commit interactive so you enter commit message
+    patch           update package version
+    push            git push to origin
+    patch-push      update package version and git push to origin - runs build and test first
+
+```
+
+## Your Project Structure
+
+The commands are opinionated by design as follows:
+
+javascript .js files are ES6 code
+use ES6 modules (import / export)
+use ./src directory for all source code
+use ./lib directory for all es5 code
+use babel to transform es6 to es5 code
+use ./dist directory for distribution code
+use engine node v6+
+conform to all npm conventions
+use npm as primary build tooling
+use mocha for testing
+
+```
+/project
+	package.json
+	src
+	lib
+	test
+	dist
+
+```
--- index.js
+++ index.js
@@ -13,48 +13,139 @@ program
 	.description('remove ./dist directory and ./dist.zip file')
 	.action(function () {
 		shell.exec("npm install", function (error, stdout, stderr) {
-			if (error !== null) {
-				// shell.exec('ls -al ./node_modules/build-scripts/gulpfile.js');
+			if (!error) {
 				shell.exec('./node_modules/gulp/bin/gulp.js clean --gulpfile ./node_modules/build-scripts/gulpfile.js');
 			}
 		});
 	});
 
 program
-	.version('0.1.0')
 	.command('zip')
 	.description('zip ./dist directory into ./dist.zip file')
 	.action(function () {
 		shell.exec("npm install", function (error, stdout, stderr) {
-			if (error !== null) {
-				// shell.exec('ls -al ./node_modules/build-scripts/gulpfile.js');
+			if (!error) {
 				shell.exec('./node_modules/gulp/bin/gulp.js zip --gulpfile ./node_modules/build-scripts/gulpfile.js');
 			}
 		});
 	});
 
 program
-	.version('0.1.0')
-	.command('build')
+	.command('build-lambda')
 	.description('build lambda function in /dist directory')
 	.action(function () {
 		shell.exec("npm install", function (error, stdout, stderr) {
-			if (error !== null) {
-				// shell.exec('ls -al ./node_modules/build-scripts/gulpfile.js');
+			if (!error) {
 				shell.exec('./node_modules/gulp/bin/gulp.js build --gulpfile ./node_modules/build-scripts/gulpfile.js');
 			}
 		});
 	});
 
 program
-	.version('0.1.0')
 	.command('upload-lambda')
 	.description('build and deploy function to aws lambda - function must already exist')
 	.action(function (env) {
 		shell.exec("npm install", function (error, stdout, stderr) {
-			if (error !== null) {
-				// shell.exec('ls -al ./node_modules/build-scripts/gulpfile.js');
+			if (!error) {
 				shell.exec('./node_modules/gulp/bin/gulp.js upload --gulpfile ./node_modules/build-scripts/gulpfile.js');
+			}
+		});
+	});
+
+/*
+	  "build": "babel src --out-dir lib",
+ */
+program
+	.command('build')
+	.description('runs babel src -o lib // src/es6 to lib/es5')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run build');
+			}
+		});
+	});
+
+/*
+	  "test": "mocha --compilers js:babel-core/register test",
+ */
+program
+	.command('test')
+	.description('runs mocha test')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('run npm test');
+			}
+		});
+	});
+
+/*
+	  "test:watch": "mocha --compilers js:babel-core/register --watch test",
+ */
+program
+	.command('test-watch')
+	.description('runs mocha test with the watch option')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run test:watch');
+			}
+		});
+	});
+
+/*
+	  "commit": "npm run build && npm run test && git commit",
+ */
+program
+	.command('commit')
+	.description('runs build, test and commit interactive so you enter commit message')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run commit');
+			}
+		});
+	});
+
+/*
+	  "patch": "npm version patch",
+ */
+program
+	.command('patch')
+	.description('update package version')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run patch');
+			}
+		});
+	});
+
+/*
+	  "push": "git push origin --follow-tags",
+ */
+program
+	.command('push')
+	.description('git push to origin')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run push');
+			}
+		});
+	});
+
+/*
+	  "patch:push": "npm run build && npm run test && npm run patch && npm run push"
+ */
+program
+	.command('patch-push')
+	.description('update package version and git push to origin - runs build and test first')
+	.action(function (env) {
+		shell.exec("npm install", function (error, stdout, stderr) {
+			if (!error) {
+				shell.exec('npm run patch:push');
 			}
 		});
 	});
