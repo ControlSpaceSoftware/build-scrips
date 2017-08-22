aef80c051c9730813372c6b71e3527896c0a4092
javadoug <doug@onvelocity.com>
2017-08-22 09:38:58 -0400

    wip

--- info.js
+++ info.js
@@ -4,7 +4,10 @@ const commands = Array.from(require('./commands.json'));
 const scripts = commands.reduce((scripts, command) => {
 	scripts[command.command] = `build-scripts ${command.command}`;
 	return scripts;
-}, {});
+}, {
+	help: '-h',
+	scripts: 'node info'
+});
 
 console.log('Add scripts to your package.json file:');
 console.log(JSON.stringify(scripts, null, 5));
